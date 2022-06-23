66d3820adb28577c8d2d85dc7ebee33b92c1d7b3
Nayara Gibbini <nayaragbn@gmail.com>
2022-06-22 21:26:53 -0300

    Primeiro commit.

---
+++ POO/src/principal/MinhaPrimeiraClasse.java
@@ -0,0 +1,18 @@
+package principal;
+
+public class MinhaPrimeiraClasse {
+// classe publica
+	
+	public static void main(String[] args) {
+	// público - sando este modificador o método pode ser acessado por qualquer classe dentro (e fora) do projeto.
+	// static - isso quer dizer que a classe não precisa ser instanciada para chamar este método.
+	//void - o método não dá retorno nenhum. Os métodos podem retornar qualquer tipo do seu projeto, até mesmo os criados por você.
+	// main - 	
+		
+		String texto = "turma";
+		System.out.println("Olá " + texto);
+		
+		
+	}
+
+}
